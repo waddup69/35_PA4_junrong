15800b0d8cbece88d159c5229c4e3a8111d8d5f1
waddup69 <pohjunrong13@gmail.com>
2020-04-30 14:17:26 +0800

    owo

---
+++ 35_junrong_PA4/Assets/Script/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= 1)
+        {
+            SceneManager.LoadScene(1);
+        }  
+    }
+}
